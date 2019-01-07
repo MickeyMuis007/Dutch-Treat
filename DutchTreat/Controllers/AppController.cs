d4941e1101843e3f2060015528a2d1df52bfe865
Michael Alan Hendricks <michaelh@lawforall.co.za>
2019-01-07 11:58:12 +0200

    Using Validation

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -24,6 +24,15 @@ namespace DutchTreat.Controllers
         [HttpPost("contact")]
         public IActionResult Contact(ContactViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                // Send the email
+            }
+            else
+            {
+                // Show the errors
+            }
+
             return View();
         }
 
