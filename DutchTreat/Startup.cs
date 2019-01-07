c1aa5b701c143609911258bec0b976c25139ca9f
Michael Alan Hendricks <michaelh@lawforall.co.za>
2019-01-07 12:31:38 +0200

    Adding a Service

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,9 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
+            // Support for real mail service
+
             services.AddMvc();
         }
 
@@ -42,3 +46,16 @@ namespace DutchTreat
         }
     }
 }
+
+/*
+ * Notes:
+ * There is 3 types of services you can add:
+ *  - var IServiceCollection services
+ *  - services.AddTransient
+ *      - This is services that doesn't not of data on itself. 
+ *      - This is often just methods that do things.
+ *  - services.AddScoped
+ *      - This services are kept a live according to the length of the connection.
+ *  - services.AddSingleton
+ *      - This is services the are only created once and are kept for the live time of the server being up.
+ */
