8cbcf87677c8f3849a222959f18840e696ff62fa
Michael Alan Hendricks <michaelh@lawforall.co.za>
2019-01-07 22:22:08 +0200

    Returning Data

---
+++ DutchTreat/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using DutchTreat.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(IDutchRepository repository, ILogger<OrdersController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+    }
+}
