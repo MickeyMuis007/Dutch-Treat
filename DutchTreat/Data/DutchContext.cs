446b3e7be989257ece86b1269219eed544655376
Michael Alan Hendricks <michaelh@lawforall.co.za>
2019-01-07 15:18:21 +0200

    Using Configuration

--- DutchTreat/Data/DutchContext.cs
+++ DutchTreat/Data/DutchContext.cs
@@ -10,6 +10,18 @@ namespace DutchTreat.Data
 {
     public class DutchContext : DbContext
     {
+        /// <summary>
+        /// DbcontextOptions is way the connection string is passed into the options.
+        /// Which is passed through to the base class.
+        /// It takes the options specified in the startup, and passing it into the context so it 
+        /// knows what connection string to use.
+        /// </summary>
+        /// <param name="options">Option specified in the startup</param>
+        public DutchContext(DbContextOptions<DutchContext> options): base(options)
+        {
+
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
     }
