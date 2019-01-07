508833759917635587d08b56e2b74196fa4137df
Michael Alan Hendricks <michaelh@lawforall.co.za>
2019-01-07 08:15:09 +0200

    Introducing jQuery

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,12 +1,23 @@
 ﻿console.log("Hi There");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+var theForm = $("#theForm");
+theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
+var button = $("#buyButton");
+button.on("click", function () {
     console.log("Buying Item");
 });
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children;
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
+
+
+
+/* 
+ Notes: 
+ - jQuery uses CSS selectors for searching for html elements.
+    - Meaning the way CSS find elements to munipulate.
+ - Getting jQuery object. It's either you use jQuery the entire word or a $.
+ */
